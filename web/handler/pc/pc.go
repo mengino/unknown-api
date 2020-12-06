1ccadeae4ccdc0c541b63212a01ef565edff3490
mengino <mengino@outlook.com>
2020-12-06 13:36:13 +0800

    fix

--- web/handler/pc/pc.go
+++ web/handler/pc/pc.go
@@ -3,22 +3,30 @@ package pc
 import (
 	"dlsite/internal/http/response"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type news struct {
-	id    int
-	title string
-	img   string
-	desc  string
+	ID        int
+	Title     string
+	Img       string
+	Desc      string
+	CreatedAt string
 }
 
 // Index 首页
 func Index(c *gin.Context) {
 	response.HTML(c, http.StatusOK, "index.html", gin.H{
-		"nav":  "index",
-		"news": []news{{id: 1, title: "xx", img: "xx", desc: "xx"}},
+		"nav": "index",
+		"news": []news{{
+			ID:        1,
+			Title:     "微信信息发出去对方没收到",
+			Img:       "https://img.homevv.com/uploadimg/ico/2019/0605/1559728456211488.jpg",
+			Desc:      "微信作为大家常用的一款聊天软件，基本上大部分人都在用吧~而就在刚在，突然发生了一件奇怪的事！自己发出去的信息，也没有被拒收，明明发送成功了，可是对方却没有收到！？这是怎么回事！天呐撸，下面就让小编来为大家介绍一下吧！",
+			CreatedAt: time.Now().Format("2006-01-02"),
+		}},
 	})
 }
 
