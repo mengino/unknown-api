60978995f5528ce941fda8af7d109239ba63b828
mengino <mengino@outlook.com>
2020-12-09 18:37:06 +0800

    update

--- web/handler/pc/pc.go
+++ web/handler/pc/pc.go
@@ -11,22 +11,65 @@ import (
 type news struct {
 	ID        int
 	Title     string
-	Img       string
+	Image     string
 	Desc      string
-	CreatedAt string
+	Total     int
+	CreatedAt time.Time
+}
+type category struct {
+	ID        int
+	Title     string
+	CreatedAt time.Time
 }
 
 // Index 首页
 func Index(c *gin.Context) {
 	response.HTML(c, http.StatusOK, "index.html", gin.H{
 		"nav": "index",
-		"news": []news{{
-			ID:        1,
-			Title:     "微信信息发出去对方没收到",
-			Img:       "https://img.homevv.com/uploadimg/ico/2019/0605/1559728456211488.jpg",
-			Desc:      "微信作为大家常用的一款聊天软件，基本上大部分人都在用吧~而就在刚在，突然发生了一件奇怪的事！自己发出去的信息，也没有被拒收，明明发送成功了，可是对方却没有收到！？这是怎么回事！天呐撸，下面就让小编来为大家介绍一下吧！",
-			CreatedAt: time.Now().Format("2006-01-02"),
-		}},
+		"news": []news{
+			{
+				ID:        1,
+				Title:     "微信信息发出去对方没收到",
+				Image:     "https://img.homevv.com/uploadimg/ico/2019/0605/1559728456211488.jpg",
+				Desc:      "微信作为大家常用的一款聊天软件，基本上大部分人都在用吧~而就在刚在，突然发生了一件奇怪的事！自己发出去的信息，也没有被拒收，明明发送成功了，可是对方却没有收到！？这是怎么回事！天呐撸，下面就让小编来为大家介绍一下吧！",
+				CreatedAt: time.Now(),
+				Total:     50,
+			},
+			{
+				ID:        2,
+				Title:     "xxxxxx",
+				Image:     "https://img.homevv.com/uploadimg/ico/2019/0605/1559728456211488.jpg",
+				Desc:      "微信作为大家常用的一款聊天软件，基本上大部分人都在用吧~而就在刚在，突然发生了一件奇怪的事！自己发出去的信息，也没有被拒收，明明发送成功了，可是对方却没有收到！？这是怎么回事！天呐撸，下面就让小编来为大家介绍一下吧！",
+				CreatedAt: time.Now(),
+				Total:     50,
+			},
+			{
+				ID:        3,
+				Title:     "zzzzzz",
+				Image:     "https://img.homevv.com/uploadimg/ico/2019/0605/1559728456211488.jpg",
+				Desc:      "微信作为大家常用的一款聊天软件，基本上大部分人都在用吧~而就在刚在，突然发生了一件奇怪的事！自己发出去的信息，也没有被拒收，明明发送成功了，可是对方却没有收到！？这是怎么回事！天呐撸，下面就让小编来为大家介绍一下吧！",
+				CreatedAt: time.Now(),
+				Total:     50,
+			},
+		},
+		"category": []category{
+			{
+				ID:    1,
+				Title: "角色扮演",
+			},
+			{
+				ID:    2,
+				Title: "休闲益智",
+			},
+			{
+				ID:    3,
+				Title: "动作冒险",
+			},
+			{
+				ID:    4,
+				Title: "解谜闯关",
+			},
+		},
 	})
 }
 
