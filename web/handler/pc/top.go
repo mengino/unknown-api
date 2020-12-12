75dcc41522ee627d7a9166d6ec5b214eec1a66c7
mengino <mengino@outlook.com>
2020-12-12 14:52:42 +0800

    feat: add top page

--- web/handler/pc/top.go
+++ web/handler/pc/top.go
@@ -3,6 +3,7 @@ package pc
 import (
 	"dlsite/internal/http/response"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,5 +12,90 @@ import (
 func Top(c *gin.Context) {
 	response.HTML(c, http.StatusOK, "top.html", gin.H{
 		"nav": "top",
+		"list": []app{
+			{
+				ID:        1,
+				Title:     "微信信息发出去对方没收到",
+				Image:     "https://img.homevv.com/uploadimg/ico/2019/0605/1559728456211488.jpg",
+				Desc:      "浓浓的古典气息",
+				Version:   "v1.0",
+				Size:      "63MB",
+				CreatedAt: time.Now(),
+				Category: category{
+					ID:    1,
+					Title: "角色扮演",
+				},
+			},
+			{
+				ID:        2,
+				Title:     "xxxxxx",
+				Image:     "https://img.homevv.com/uploadimg/ico/2019/0605/1559728456211488.jpg",
+				Desc:      "浓浓的古典气息",
+				Version:   "v1.0",
+				Size:      "63MB",
+				CreatedAt: time.Now(),
+				Category: category{
+					ID:    1,
+					Title: "角色扮演",
+				},
+			},
+			{
+				ID:        3,
+				Title:     "zzzzzz",
+				Image:     "https://img.homevv.com/uploadimg/ico/2019/0605/1559728456211488.jpg",
+				Desc:      "浓浓的古典气息",
+				Version:   "v1.0",
+				Size:      "63MB",
+				CreatedAt: time.Now(),
+				Category: category{
+					ID:    1,
+					Title: "角色扮演",
+				},
+			},
+			{
+				ID:        4,
+				Title:     "bbbbbs",
+				Image:     "https://img.homevv.com/uploadimg/ico/2019/0605/1559728456211488.jpg",
+				Desc:      "浓浓的古典气息",
+				Version:   "v1.0",
+				Size:      "63MB",
+				CreatedAt: time.Now(),
+				Category: category{
+					ID:    1,
+					Title: "角色扮演",
+				},
+			},
+			{
+				ID:        5,
+				Title:     "aaaa",
+				Image:     "https://img.homevv.com/uploadimg/ico/2019/0605/1559728456211488.jpg",
+				Desc:      "浓浓的古典气息",
+				Version:   "v1.0",
+				Size:      "63MB",
+				CreatedAt: time.Now(),
+				Category: category{
+					ID:    1,
+					Title: "角色扮演",
+				},
+			},
+		},
+		"category": []category{
+			{
+				ID:    1,
+				Title: "角色扮演",
+			},
+			{
+				ID:    2,
+				Title: "休闲益智",
+			},
+			{
+				ID:    3,
+				Title: "动作冒险",
+			},
+			{
+				ID:    4,
+				Title: "解谜闯关",
+			},
+		},
 	})
 }
